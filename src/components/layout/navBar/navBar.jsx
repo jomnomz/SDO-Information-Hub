dd562f5b877c804780f2948c83a0747bbf5122e3
jomnomz <jomeorenzdelacruz@gmail.com>
2026-02-19 20:16:29 +0800

    SDO Information Hub

---
+++ src/components/layout/navBar/navBar.jsx
@@ -0,0 +1,30 @@
+export default function NavBar(){
+    return(
+        <div>
+            <div>
+                Memo
+            </div>
+            <div>
+                Facebook
+            </div>
+            <div>
+                Teacher Portal
+            </div>
+            <div>
+                Ranking Calculator
+            </div>
+            <div>
+                Calendar
+            </div>
+            <div>
+                Qualifications
+            </div>
+            <div>
+                Documents
+            </div>
+            <div>
+                FAQs
+            </div>
+        </div>
+    )
+}
