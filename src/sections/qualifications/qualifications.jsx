a053a86eb07709548576242ec11d69cef23f3798
jomnomz <jomeorenzdelacruz@gmail.com>
2026-02-23 19:05:26 +0800

    qualifications

--- src/sections/qualifications/qualifications.jsx
+++ src/sections/qualifications/qualifications.jsx
@@ -2,6 +2,10 @@ export default function qualifications(){
     return(
         <div>
             <div>Qualifications</div>
+
+            <div>
+                <img src="" alt="imgs" />
+            </div>
         </div>
     )
 }
